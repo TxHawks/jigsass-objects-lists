c510e327a1211128659c55bd380d9f72392d4ce5
TxHawks <tofu.hawks@gmail.com>
2016-04-04 17:02:34 +0300

    Initial commit

---
+++ gh-pages.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Generate documentation and deploy it to GitHub pages
+# http://TxHawks.github.io/jigsass-objects-lists/
+gulp sass:sg
+git add ./docs/styleguide/.
+git commit -m "Compile documentation"
+git subtree push --prefix docs origin gh-pages
